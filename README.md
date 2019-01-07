efc7293c49894848ba7d84398fa52d31836ff727
Disquietude <danny93ngo@gmail.com>
2019-01-06 22:45:00 -0500

    Added API Endpoints

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # CoinMarketCap Microservice
 
-Microservice that make requests to the CoinMarketCap API.
+Microservice that makes requests to the CoinMarketCap API.
+
+## API Endpoints
+
+### /api/map/:limit?
+
+Returns a paginated list of all cryptocurrencies by CoinMarketCap ID.
+
+Route Parameters:
+  * limit (Optional): Integer from 1-5000. Specifies the number of results to return.
+
+### /api/quotes/:ids/:convert?
+
+Returns the latest market quote for 1 or more cryptocurrencies.
+
+Route Parameters:
+  * ids: String (ex: 1,2). One or more comma-separated cryptocurrency CoinMarketCap IDs. 
+  * convert (Optional): String (ex: BTC). Converts prices to the selected currency. Must be 3 uppercase letters.
--- server.js
+++ server.js
@@ -10,30 +10,61 @@ app.use(cors({optionsSuccessStatus: 200}));
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-const requestOptions = {
-  method: 'GET',
-  uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map',
-  qs: {
-    limit: 10
-  },
-  headers: {
-    'X-CMC_PRO_API_KEY': process.env.API_KEY
-  },
-  json: true,
-  gzip: true
-};
-
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/api', (req, res) => {
+let defaultOptions = {
+  method: 'GET',
+  json: true,
+  gzip: true
+};
+
+let apiKey = {
+  headers: {
+    'X-CMC_PRO_API_KEY': process.env.API_KEY
+  }
+};
+
+let defaultUri = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/';
+
+//Get CoinMarketCap ID Map
+app.get('/api/map/:limit?', (req, res) => {
+  
+  let requestOptions = Object.assign({uri: defaultUri + 'map'}, defaultOptions, apiKey);
+
+  if (req.params.limit !== undefined) {
+    requestOptions.qs = {
+      limit: req.params.limit
+    }
+  }
+
   rp(requestOptions).then(response => {
     console.log('API call response:', response);
     res.json(response);
   }).catch((err) => {
     console.log('API call error:', err.message);
   });
+
+});
+
+//Get Market Quotes
+app.get('/api/quotes/:ids/:convert?', (req, res) => {
+
+  let requestOptions = Object.assign({uri: defaultUri + 'quotes/latest'}, defaultOptions, apiKey);
+
+  requestOptions.qs = {
+    id: req.params.ids,
+    convert: req.params.convert || 'USD'
+  }
+
+  rp(requestOptions).then(response => {
+    console.log('API call response:', response);
+    res.json(response);
+  }).catch((err) => {
+    console.log('API call error:', err.message);
+  })
+
 });
 
 app.listen(process.env.PORT, () => {
